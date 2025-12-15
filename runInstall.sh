dc6689db1a3306645a8769ce3617edb18308951a
Alexander Komyakov <al.komyakov@norsi-trans.ru>
2025-12-15 14:41:01 +0300

    run install script fix

--- runInstall.sh
+++ runInstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt update
-apt install ansible
-ansible-playbook playbook.yml -i localhost, -e user_name=alexander -e user_password=password
+apt install ansible sudo sshpass -y
+ansible-playbook playbook.yml -i localhost -e user_name=alexander -e user_password=password -e ansible_ssh_user=alexander -e ansible_ssh_password=password -e ansible_become_method=su
