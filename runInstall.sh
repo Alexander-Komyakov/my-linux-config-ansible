fd87caf5367f65d94ed17f76800b0e0855cda77c
Alexander Komyakov <al.komyakov@norsi-trans.ru>
2025-12-16 01:33:43 +0300

    fix count monitor

--- runInstall.sh
+++ runInstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt update
 apt install ansible sudo sshpass -y
-export COUNT_MONITOR=`ls /sys/class/drm/*/status | xargs grep -H "connected" | wc -l`
+#export COUNT_MONITOR=`ls /sys/class/drm/*/status | xargs grep -H "connected" | wc -l`
+export COUNT_MONITOR=1
 ansible-playbook playbook.yml -i localhost -e user_name=alexander -e user_password=password -e ansible_ssh_user=alexander -e ansible_ssh_password=password -e ansible_become_method=su -e count_monitor=$COUNT_MONITOR
